331261739db50f383044aff889115cae76ad8067
wez <wez@users.noreply.github.com>
2020-06-08 17:33:36 +0000

    Automated update to match latest tag

--- Formula/wezterm.rb
+++ Formula/wezterm.rb
@@ -1,11 +1,18 @@
+# Note: if you are viewing this from the tap repo, this file is automatically
+# updated from:
+# https://github.com/wez/wezterm/blob/master/ci/wezterm-homebrew-macos.rb.template
+# by automation in the wezterm repo.
+
 class Wezterm < Formula
   desc "A GPU-accelerated cross-platform terminal emulator and multiplexer written by @wez and implemented in Rust"
   homepage "https://wezfurlong.org/wezterm/"
-  url "https://github.com/wez/wezterm/releases/download/20200517-122836-92c201c6/WezTerm-macos-20200517-122836-92c201c6.zip"
-  sha256 "afd3d83d35b0c9b1b476d36810ab1c0098f562e0edc52bc3da2be98b4cf1976f"
+  url "https://github.com/wez/wezterm/releases/download/20200608-102924-d979a63/WezTerm-macos-@TAG@.zip"
+  sha256 "3b76d870f181c95072e77939902bf70ebd8b15a08348db73c7f949b68d55d4d2"
 
   def install
     prefix.install "WezTerm.app"
     bin.write_exec_script "#{prefix}/WezTerm.app/wezterm"
+    bin.write_exec_script "#{prefix}/WezTerm.app/strip-ansi-escapes"
   end
 end
+
