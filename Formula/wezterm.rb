6a8e1483222aad92b29740d4033fb9dbcb10cae5
Wez Furlong <wez@wezfurlong.org>
2020-06-11 13:56:13 -0700

    allow --head installs

--- Formula/wezterm.rb
+++ Formula/wezterm.rb
@@ -8,6 +8,7 @@ class Wezterm < Formula
   homepage "https://wezfurlong.org/wezterm/"
   url "https://github.com/wez/wezterm/releases/download/20200608-110940-3fb3a61/WezTerm-macos-20200608-110940-3fb3a61.zip"
   sha256 "40bb259fec6f44f864e5f9480c40901d0e9a8fb07ef534b45dd78ca615b38d72"
+  head "https://github.com/wez/wezterm/releases/download/nightly/WezTerm-macos-nightly.zip"
 
   def install
     prefix.install "WezTerm.app"
