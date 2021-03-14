1ab54a32a4e0f3caa2770a2fd19ff99f104009c3
wez <wez@users.noreply.github.com>
2021-03-14 19:10:23 +0000

    Automated update to match latest tag

--- Formula/wezterm.rb
+++ Formula/wezterm.rb
@@ -1,13 +1,13 @@
 # Note: if you are viewing this from the tap repo, this file is automatically
 # updated from:
-# https://github.com/wez/wezterm/blob/master/ci/wezterm-homebrew-macos.rb.template
+# https://github.com/wez/wezterm/blob/main/ci/wezterm-homebrew-macos.rb.template
 # by automation in the wezterm repo.
 
 class Wezterm < Formula
   desc "A GPU-accelerated cross-platform terminal emulator and multiplexer written by @wez and implemented in Rust"
   homepage "https://wezfurlong.org/wezterm/"
-  url "https://github.com/wez/wezterm/releases/download/20210203-095643-70a364eb/WezTerm-macos-20210203-095643-70a364eb.zip"
-  sha256 "b981e356bc7323e23b5f6b767b10fd530737878529aa70c26252438abb1bfc24"
+  url "https://github.com/wez/wezterm/releases/download/20210314-114017-04b7cedd/WezTerm-macos-20210314-114017-04b7cedd.zip"
+  sha256 "f45a51aa590382be88594db5d5767ab58da56ff12451b1fb1baabb3bf5f58b9d"
   head "https://github.com/wez/wezterm/releases/download/nightly/WezTerm-macos-nightly.zip"
 
   def install
@@ -22,3 +22,4 @@ class Wezterm < Formula
     }
   end
 end
+
