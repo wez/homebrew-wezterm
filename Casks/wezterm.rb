f19c0a2af5d5f3884e5bac1cd5ecbcf399970884
laggardkernel <laggardkernel@gmail.com>
2021-05-22 15:42:23 +0800

    feat: add Casks wezterm, wezterm-nightly for GUI app installation

---
+++ Casks/wezterm.rb
@@ -0,0 +1,48 @@
+cask "wezterm" do
+  version "20210502-154244-3f7122cb"
+  sha256 "be8e235d5bf6b2876a993f8c084cd7bfc3cf8121e7aa99038d9209ee87dd9118"
+
+  url "https://github.com/wez/wezterm/releases/download/#{version}/WezTerm-macos-#{version}.zip"
+  name "WezTerm"
+  desc "A GPU-accelerated cross-platform terminal emulator and multiplexer written by @wez and implemented in Rust"
+  homepage "https://wezfurlong.org/wezterm/"
+
+  conflicts_with cask: "wez/wezterm/wezterm-nightly"
+
+  # Unclear what the minimal OS version is
+  # depends_on macos: ">= :sierra"
+
+  app "WezTerm.app"
+  [
+    "wezterm",
+    "wezterm-gui",
+    "wezterm-mux-server",
+    "strip-ansi-escapes"
+  ].each do |tool|
+    binary "#{appdir}/WezTerm.app/Contents/MacOS/#{tool}"
+  end
+
+  preflight do
+    # Move "WezTerm-macos-#{version}/WezTerm.app" out of the subfolder
+    staged_subfolder = staged_path.glob(["WezTerm-*", "wezterm-*"]).first
+    if staged_subfolder
+      FileUtils.mv(staged_subfolder/"WezTerm.app", staged_path)
+      FileUtils.rm_rf(staged_subfolder)
+    end
+  end
+
+  zap trash: [
+    "~/.config/wezterm",
+    "~/Library/Saved Application State/com.github.wez.wezterm.savedState",
+  ]
+
+  def caveats; <<~EOS
+    Cask #{token} related executables like 'wezterm', 'wezterm-gui',
+    'wezterm-mux-server', are linked into
+      /usr/local/bin/    for x86 Mac,
+      /opt/homebrew/bin/ for M1 Mac.
+
+    Removal of them is ensured by 'brew uninstall --cask #{token}'.
+  EOS
+  end
+end
